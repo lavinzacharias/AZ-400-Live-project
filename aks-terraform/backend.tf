e80c7bd713daea5939e2cdf69bc888531e10ccdc
lavin <lavin@cloudpoint.in>
2025-01-16 19:16:45 +0530

    message

--- aks-terraform/backend.tf
+++ aks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name = "deploy-first-containerapp-rg"
-    storage_account_name= "container633053"
+    storage_account_name= "terraformstate3053"
     container_name = "tfstate"
     key = "terraform.tfstate"
   }
